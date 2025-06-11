f2b8887a9a720ace50fd91e1181a472d8651eca2
rackaaf <rackaaf@gmail.com>
2025-06-12 02:43:27 +0700

    v4

--- Klien/build.gradle.kts
+++ Klien/build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath("io.github.cdimascio:dotenv-kotlin:6.4.1")
+    }
 }
